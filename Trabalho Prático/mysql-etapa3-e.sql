99f7fa49d8a45d4c4400662dfcdaa3ab3ac4f678
Lucas de Castro <100042150+LuckZeera@users.noreply.github.com>
2023-07-13 20:12:20 -0300

    Update mysql-etapa3-e.sql

--- Trabalho Prático/mysql-etapa3-e.sql
+++ Trabalho Prático/mysql-etapa3-e.sql
@@ -7,7 +7,7 @@ DELETE FROM Carro WHERE idCarro = 1;
 DELETE FROM Endereco WHERE idEndereco = 1;
 
 -- Exclusão de dados na tabela Engenheiro
-DELETE FROM Engenheiro WHERE idFuncionario = 1;
+DELETE FROM Engenheiro WHERE idEngenheiro = 1;
 
 -- Exclusão de dados na tabela Equipe
 DELETE FROM Equipe WHERE idEquipe = 1;
@@ -15,5 +15,5 @@ DELETE FROM Equipe WHERE idEquipe = 1;
 -- Exclusão de dados em tabelas relacionadas usando DELETE aninhado
 DELETE FROM Funcionario 
 	   WHERE idEquipe IN (SELECT idEquipe 
-						  FROM Equipe 
-                          WHERE nomeEquipe = 'Mercedes-AMG Petronas Formula One Team');
+			      FROM Equipe 
+                              WHERE nomeEquipe = 'Mercedes-AMG Petronas Formula One Team');
