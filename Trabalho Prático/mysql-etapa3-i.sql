fac3fc13fb300475dca93fbf50e94058113e132e
Lucas de Castro <100042150+LuckZeera@users.noreply.github.com>
2023-07-13 21:10:45 -0300

    Update mysql-etapa3-i.sql

--- Trabalho Prático/mysql-etapa3-i.sql
+++ Trabalho Prático/mysql-etapa3-i.sql
@@ -1,7 +1,6 @@
 USE F1;
 
 DELIMITER $$
-
 CREATE TRIGGER before_insert_funcionario
 BEFORE INSERT ON Funcionario FOR EACH ROW
 BEGIN
@@ -9,7 +8,9 @@ BEGIN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Aceita-se somente M(Masculino) e F(Feminino) para o sexo do funcionário.';
     END IF;
 END $$
+DELIMITER ;
 
+DELIMITER $$
 CREATE TRIGGER before_update_equipe
 BEFORE UPDATE ON Equipe FOR EACH ROW
 BEGIN
@@ -23,7 +24,9 @@ BEGIN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Número de títulos menor que o atual.';
     END IF;
 END $$
+DELIMITER ;
 
+DELIMITER $$
 CREATE TRIGGER before_delete_equipe
 BEFORE DELETE ON Equipe FOR EACH ROW
 BEGIN
@@ -31,6 +34,32 @@ BEGIN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Não é possível deletar a equipe da Mercedes.';
     END IF;
 END $$
-
 DELIMITER ;
 
+-- Inserção de um Funcionário informando o sexo corretamente (ocorre normalmente)
+INSERT INTO Funcionario (idFuncionario, nome, nacionalidade, sexo, tipo_func, ano_contratacao, idEquipe)
+VALUES (11, 'Ana Silva', 'Brasil', 'F', 'E', 2022, 1);
+
+-- Inserção de um Funcionário informando o sexo errado
+INSERT INTO Funcionario (idFuncionario, nome, nacionalidade, sexo, tipo_func, ano_contratacao, idEquipe)
+VALUES (11, 'Ana Silva', 'Brasil', 'G', 'E', 2022, 1);
+
+
+-- Atualizando o número de títulos de uma equipe por um número maior (ocorre normalmente)
+UPDATE Equipe
+SET titulos = 8
+WHERE idEquipe = 1;
+
+-- Tentando atualizar o número de títulos de uma equipe por um número menor
+UPDATE Equipe
+SET titulos = 3
+WHERE idEquipe = 2;
+
+
+-- Excluindo uma outra equipe (funciona normalmente)
+DELETE FROM Equipe
+WHERE idEquipe = 2;
+
+-- Tentando excluir a equipe Mercedes
+DELETE FROM Equipe
+WHERE idEquipe = 1;
